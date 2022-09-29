eab55f2da9150c587972199e73cf12534dd73cd7
Siddhartha Arora <110627236+Siddharthaaa21@users.noreply.github.com>
2022-09-29 23:30:50 +0530

    Update Tow_sum.java

--- leetcode/Tow_sum.java
+++ leetcode/Tow_sum.java
@@ -6,13 +6,15 @@ class Solution {
         int a[]=new int[2];
         for(int i=0;i<nums.length;i++)
         {
-            for(int j =i+1;j<nums.length;j++){
+            for(int j =i+1;j<nums.length;j++)
+            {
                 if(target==(nums[i]+nums[j]))
                 {
                     a[0]=i;
-                    a[1]=j;}
-                 }
-        }return a;
+                    a[1]=j;
+                }
+            }
+            }return a;
     }}
 
 
