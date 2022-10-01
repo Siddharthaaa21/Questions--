f0c573d6969fe467decb5ed0b153ce8bf73965d3
codeelevate321 <500098457@stu.upes.ac.in>
2022-10-01 23:39:46 +0530

    added in Leetcode

---
+++ leetcode/isPalindrome.java
@@ -0,0 +1,24 @@
+
+class isPalindrome {
+    public boolean result(int x) {
+        StringBuffer str = new StringBuffer(Interger.toString(x));
+
+        if ((str.reverse().toString().equals(str.toString())))
+            return (true);
+        else
+            return false;
+    }}//this is not the fastest way tho.
+  //class Solution {
+//    public boolean isPalindrome(int x) {
+//        StringBuffer sb=new StringBuffer(Integer.toString(x));
+//        return (sb.toString().compareTo(sb.reverse().toString())==0)?true:false;
+//    }
+//}
+//class Solution {
+//    public boolean isPalindrome(int x) {
+//       StringBuffer str=new StringBuffer(Integer.toString(x));
+//
+//                    return (str.toString()).equals(str.reverse().toString());
+//
+//    }
+//}
