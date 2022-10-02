63d21c7a9d79b658ef3bf6362a41da8ae562ae47
Siddhartha Arora <110627236+Siddharthaaa21@users.noreply.github.com>
2022-10-02 23:25:23 +0530

    Update isPalindrome.java

--- leetcode/isPalindrome.java
+++ leetcode/isPalindrome.java
@@ -8,12 +8,7 @@ class Solution {
         else
             return false;
     }}//this is not the fastest way tho.
-  //class Solution {
-//    public boolean isPalindrome(int x) {
-//        StringBuffer sb=new StringBuffer(Integer.toString(x));
-//        return (sb.toString().compareTo(sb.reverse().toString())==0)?true:false;
-//    }
-//}
+ 
 //class Solution {
 //    public boolean isPalindrome(int x) {
 //       StringBuffer str=new StringBuffer(Integer.toString(x));
